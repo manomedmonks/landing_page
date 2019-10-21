ca7f260e0f3ac87b86c5af5c2ac8e034edbda96a
manomedmonks <47103335+manomedmonks@users.noreply.github.com>
2019-10-21 16:02:28 +0530

    uploaded lab landing page files

---
+++ lab/assets-2/js/custom.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+
+  var header_height = $("header").innerHeight();
+  //$(".banner-row").css("padding-top", header_height);
+  $('.testimonial').owlCarousel({
+    items:1,
+    nav:true,
+    autoplay:true,
+    loop:true,
+  });
+
+  $(".free-call-button,.book-now-button a,.footer-contact ul li a").click(function(){
+    $("body").addClass("active-landing-form"),
+    setTimeout(function() {
+      $('#landing-form input[type="text"]').focus();
+    }, 1000);
+  });
+
+  $(".remove-sidebar").click(function(){
+    $("body").removeClass("active-landing-form");
+  });
+
+  $(".navbar-toggler").click(function(){
+    $(this).toggleClass("actie-navar");
+  });
+
+  $(".remove-chat").click(function(){
+    $(this).hide();
+    $(".chating-box").hide();
+  });
+
+  setInterval(function () {
+    $('.chating-box').css('transform', 'scale(1)');
+    $('.remove-chat').css('transform', 'scale(1)');
+  }, 2000);
+
+
+  if ($(window).width() > 768) {
+    $('.free-call-button,.book-now-button a,.footer-contact ul li a').on('click', function(event) {
+      var target = $($(this).attr('href'));
+      if (target.length) { event.preventDefault();
+        $('html, body').animate({           
+          scrollTop: target.offset().top - header_height },  500)
+      }
+    });
+  }
+});
+
+
+
+
